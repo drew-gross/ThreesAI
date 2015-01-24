fee9a113be790b511f1c80defe8e7e53337ea81c
Drew Gross <drew.a.gross@gmail.com>
2015-01-23 23:37:18 -0800

    better printing of board

--- ThreesAI/ThreesBoard.cpp
+++ ThreesAI/ThreesBoard.cpp
@@ -14,6 +14,9 @@ ThreesBoard::ThreesBoard() {
 }
 
 std::ostream& operator<<(std::ostream &os, ThreesBoard board){
-    os << "---";
+    os << "|" << board.board[0][0] << "|" << board.board[0][1] << "|" << board.board[0][2] << "|" << board.board[0][3] << "|" << std::endl;
+    os << "|" << board.board[1][0] << "|" << board.board[1][1] << "|" << board.board[1][2] << "|" << board.board[1][3] << "|" << std::endl;
+    os << "|" << board.board[2][0] << "|" << board.board[2][1] << "|" << board.board[2][2] << "|" << board.board[2][3] << "|" << std::endl;
+    os << "|" << board.board[3][0] << "|" << board.board[3][1] << "|" << board.board[3][2] << "|" << board.board[3][3] << "|" << std::endl;
     return os;
 }
