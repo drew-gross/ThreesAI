d5ae6ce789ecd3508bbe11b7cf41ba2e7f551d20
Drew Gross <drew.a.gross@gmail.com>
2015-01-23 17:24:21 -0800

    Initial Commit

---
+++ ThreesAI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ThreesAI
+//
+//  Created by Drew Gross on 2015-01-23.
+//  Copyright (c) 2015 DrewGross. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
