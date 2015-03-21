9553e8c1458e18ed0d10541b272f9bc69ee8e0f7
Drew Gross <drew.a.gross@gmail.com>
2015-03-20 21:28:22 -0700

    profiling

--- ThreesAI/main.cpp
+++ ThreesAI/main.cpp
@@ -7,6 +7,9 @@
 //
 
 #include <iostream>
+
+#include "Logging.h"
+
 #include "ThreesBoard.h"
 #include "RandomAI.h"
 #include "ZeroDepthMaxScoreAI.h"
@@ -19,10 +22,14 @@ int main(int argc, const char * argv[]) {
     random_device rd;
     TileStack::randomGenerator.seed(0);
     ExpectimaxAI ai;
+    clock_t startTime = clock();
     while (!ai.board.isGameOver()) {
         cout << ai.board << endl;
         Direction move = ai.playTurn();
         cout << move << endl;
     }
+    clock_t endTime = clock();
+    double elapsed_time = (endTime-startTime)/(double)CLOCKS_PER_SEC;
+    MYLOG(elapsed_time);
     return 0;
 }
