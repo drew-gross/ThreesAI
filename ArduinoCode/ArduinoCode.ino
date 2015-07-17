1c94764bcfaf5ef1ea474cd43976a1745e191fa4
Drew Gross <drew.a.gross@gmail.com>
2015-07-16 22:16:14 -0700

    Make arduino default to not pressing position

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -1,9 +1,9 @@
-/* 
+/*
 This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
 It won't work with v1.x motor shields! Only for the v2's with built in PWM
 control
 
-For use with the Adafruit Motor Shield v2 
+For use with the Adafruit Motor Shield v2
 ----> http://www.adafruit.com/products/1438
 */
 
@@ -13,7 +13,7 @@ For use with the Adafruit Motor Shield v2
 #include "utility/Adafruit_PWMServoDriver.h"
 #include <Servo.h>
 
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_StepperMotor *udMotor = AFMS.getStepper(200, 2);
 Adafruit_StepperMotor *lrMotor = AFMS.getStepper(200, 1);
 
@@ -23,10 +23,11 @@ void setup() {
   Serial.begin(9600);
 
   AFMS.begin();
-  
+
   udMotor->setSpeed(50);
   lrMotor->setSpeed(50);
   buttonPresser.attach(9);
+  buttonPresser.write(180);
 }
 
 void loop() {
@@ -39,8 +40,8 @@ void loop() {
       case 'd':
         udMotor->step(200, BACKWARD, DOUBLE);
         break;
-      case 'l':  
-        lrMotor->step(200, FORWARD, DOUBLE); 
+      case 'l':
+        lrMotor->step(200, FORWARD, DOUBLE);
         break;
       case 'r':
         lrMotor->step(200, BACKWARD, DOUBLE);
@@ -48,7 +49,7 @@ void loop() {
       case 'b':
         buttonPresser.write(0);
         delay(500);
-        buttonPresser.write(180); 
+        buttonPresser.write(180);
     }
   }
 }
