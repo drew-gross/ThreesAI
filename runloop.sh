82c10b4ff759a627b8eff738d97e023535231d94
Drew Gross <drew.a.gross@gmail.com>
2016-01-10 18:08:12 -0800

    devnull

--- runloop.sh
+++ runloop.sh
@@ -2,6 +2,6 @@ while true
 do
 	git pull
 	pod install
-	xcodebuild -workspace ThreesAI.xcworkspace -scheme "Release ThreesAI"
+	xcodebuild -workspace ThreesAI.xcworkspace -scheme "Release ThreesAI" > /dev/null
 	./build/Products/Release/ThreesAI
 done
