3e6d60763b8354be17d84741f528047134d72635
Drew Gross <drew.a.gross@gmail.com>
2016-01-16 20:56:04 -0800

    Remove dupe symbol

--- ThreesAI/AdaptiveDepthAI.cpp
+++ ThreesAI/AdaptiveDepthAI.cpp
@@ -22,41 +22,13 @@ AdaptiveDepthAI::AdaptiveDepthAI(BoardStateCPtr board, unique_ptr<BoardOutput> o
 void AdaptiveDepthAI::receiveState(Direction d, BoardState const & newState) {};
 void AdaptiveDepthAI::prepareDirection() {};
 
-float getExpectedScoreIfMovedInDirection(BoardState const& justMovedBoard, Direction d, uint8_t depth, Heuristic h) {
-    auto allAdditions = justMovedBoard.possibleAdditions(d);
-    float score = 0;
-    for (auto&& info : allAdditions) {
-        BoardState potentialBoard(BoardState::AddSpecificTile(d, info.i, info.t), justMovedBoard, true);
-        if (depth == 0) {
-            score += h(potentialBoard)*info.probability;
-        } else {
-            vector<pair<Direction, float>> scoresForMoves;
-            for (auto&& d : allDirections) {
-                if (potentialBoard.isMoveValid(d)) {
-                    BoardState movedBoard(BoardState::MoveWithoutAdd(d), potentialBoard);
-                    scoresForMoves.push_back({d, getExpectedScoreIfMovedInDirection(movedBoard, d, depth - 1, h)});
-                }
-            }
-            if (scoresForMoves.empty()) {
-                score += potentialBoard.score();
-            } else {
-                score += max_element(scoresForMoves.begin(), scoresForMoves.end(), [](pair<Direction, float> left, pair<Direction, float> right){
-                    return left.second < right.second;
-                })->second;
-            }
-        }
-    }
-    return score;
-}
-
-
 Direction AdaptiveDepthAI::getDirection() const {
     vector<pair<Direction, float>> scoresForMoves;
     
     for (auto&& d : allDirections) {
         if (this->currentState()->isMoveValid(d)) {
             BoardState movedBoard(BoardState::MoveWithoutAdd(d), *this->currentState());
-            scoresForMoves.push_back({d, getExpectedScoreIfMovedInDirection(movedBoard, d, this->depth, this->heuristic)});
+            //scoresForMoves.push_back({d, getExpectedScoreIfMovedInDirection(movedBoard, d, this->depth, this->heuristic)});
         }
     }
     debug(scoresForMoves.empty());
